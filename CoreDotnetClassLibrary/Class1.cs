42acd6c766c26d6a47ddcabecfbc9ad0f9219dda
Preeti Mahajan <preeti.a.mahajan@accenture.com>
2023-03-13 08:46:07 +0000

    Updated Class1.cs

--- CoreDotnetClassLibrary/Class1.cs
+++ CoreDotnetClassLibrary/Class1.cs
@@ -14,6 +14,14 @@ namespace CoreDotnetClassLibrary
             string password = "Admin123"; // Sensitive
             string usernamePassword  = "user=admin&password=Admin123"; // Sensitive
             string url = "scheme://user:Admin123@domain.com"; // Sensitive
+
+            var urlHttp = "http://example.com";                 // Noncompliant
+            var urlFtp = "ftp://anonymous@example.com";         // Noncompliant
+            var urlTelnet = "telnet://anonymous@example.com";   // Noncompliant
+
+            using var smtp = new SmtpClient("host", 25); // Noncompliant, EnableSsl is not set
+            using var telnet = new MyTelnet.Client("host", port); // Noncompliant, rule raises Security Hotspot on any member containing "Telnet"
+            
             byte[] image = null;
             if (!string.IsNullOrEmpty(filePath))
             {
