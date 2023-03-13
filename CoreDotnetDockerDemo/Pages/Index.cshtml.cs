0ead4847ca2857941616a6c1826b091f486df6ae
Preeti Mahajan <preeti.a.mahajan@accenture.com>
2023-03-13 10:55:01 +0000

    Updated Index.cshtml.cs

--- CoreDotnetDockerDemo/Pages/Index.cshtml.cs
+++ CoreDotnetDockerDemo/Pages/Index.cshtml.cs
@@ -15,7 +15,7 @@ namespace CoreDotnetDockerDemo.Pages
         public void OnGet()
         {
             this.Message = "This is my First ASP.Net Core Razor Page";
-            string fileName = "test.html"
+            string fileName = "test.html";
             System.IO.File.Delete(fileName); // Noncompliant
             return Content("File " + fileName + " deleted");
         }
