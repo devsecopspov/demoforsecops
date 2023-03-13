328170379d4f8dcdd3228740a3e30ee30607b1c3
Preeti Mahajan <preeti.a.mahajan@accenture.com>
2023-03-13 07:48:03 +0000

    Updated Class1.cs

--- CoreDotnetClassLibrary/Class1.cs
+++ CoreDotnetClassLibrary/Class1.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.IO;
 
 namespace CoreDotnetClassLibrary
 {
     public class Class1
     {
+        public FileContentResult GetImageFromFilePath(string filePath)
+        {
+            byte[] image = null;
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                image = System.IO.File.ReadAllBytes(filePath);
+            }
+            if (image == null)
+                return null;
+            return File(image, "image/jpeg");
+        }
     }
 }
