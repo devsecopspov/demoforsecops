85455395e081b5cdd81e6138cac95fa6bb8c0e4a
Preeti Mahajan <preeti.a.mahajan@accenture.com>
2023-03-13 05:54:57 +0000

    Updated Index.cshtml.cs

--- CoreDotnetDockerDemo/Pages/Index.cshtml.cs
+++ CoreDotnetDockerDemo/Pages/Index.cshtml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Web.UI;
 
 namespace CoreDotnetDockerDemo.Pages
 {
@@ -13,6 +15,11 @@ namespace CoreDotnetDockerDemo.Pages
         public void OnGet()
         {
             this.Message = "This is my First ASP.Net Core Razor Page";
+        }
+        public object MyDeserialize(byte[] bytes)
+        {
+            LosFormatter formatter = new LosFormatter();
+            return formatter.Deserialize(new MemoryStream(bytes));
         }
     }
 }
